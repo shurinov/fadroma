87d3daf9c05e898cd432a86ec640c5c554595bfe
Adam A <adam@hack.bg>
2024-04-25 17:30:00 +0300

    wip: docs(agent): define all documentation modules

--- packages/agent/docs.ts
+++ packages/agent/docs.ts
@@ -3,7 +3,16 @@
 import { readFileSync } from 'node:fs'
 import { documentModule } from '@hackbg/docs'
 
-documentModule({
-  data: JSON.parse(readFileSync('docs.json', 'utf8')),
-  sources: ['deploy.ts']
-})
+const data = JSON.parse(readFileSync('docs.json', 'utf8'))
+
+documentModule({ data, target: 'core.md',       sources: ['core.ts'],                          })
+documentModule({ data, target: 'chain.md',      sources: ['chain.ts'],                         })
+documentModule({ data, target: 'deploy.md',     sources: ['deploy.ts'],                        })
+documentModule({ data, target: 'governance.md', sources: ['governance.ts'],                    })
+documentModule({ data, target: 'identity.md',   sources: ['identity.ts'],                      })
+documentModule({ data, target: 'program.md',    sources: ['program.browser.ts', 'program.ts'], })
+documentModule({ data, target: 'staking.md',    sources: ['staking.ts'],                       })
+documentModule({ data, target: 'store.md',      sources: ['store.ts'],                         })
+documentModule({ data, target: 'stub.md',       sources: ['stub.ts'],                          })
+documentModule({ data, target: 'token.md',      sources: ['token.ts'],                         })
+documentModule({ data, target: 'tx.md',         sources: ['tx.ts'],                            })
